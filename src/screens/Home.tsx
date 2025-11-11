2e4f06490e5e97e43874c135adaf3f26dcd7610a
KesavCodes <kesavakumarcode@outlook.com>
2025-11-11 21:51:38 +0530

    chore: update dependencies and add Nativewind setup
    - Added new dependencies for navigation and styling in package.json.
    - Created a new Home screen component using Nativewind for styling.
    - Added Tailwind CSS configuration for Nativewind integration.

---
+++ src/screens/Home.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-200">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
